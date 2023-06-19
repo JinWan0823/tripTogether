b91fe1537521a83e51a74a1e23ba25d201b1e088
jinWan0823 <dhksl823@gmail.com>
2023-06-19 20:00:33 +0900

    로그인 기능

--- trip-together/src/pages/Login.tsx
+++ trip-together/src/pages/Login.tsx
@@ -1,23 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { signInWithEmailAndPassword } from "@firebase/auth";
 import Header from "../components/Header";
 import back from "../assets/fooback.jpg";
 import LoginButton from "../components/LoginButton";
 import FooLogo from "../assets/logo.svg";
-import { auth } from "../firebase";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
+import { auth } from "../firebase";
 
 export default function Login() {
-  console.log(auth);
-
+  const userInfo = useContext(AuthContext);
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [pwd, setPwd] = useState("");
 
-  const handleEmailChange = (e) => {
+  const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
     setEmail(e.target.value);
   };
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+  const handlePwd = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setPwd(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, pwd)
+      .then(() => {
+        alert("로그인 성공");
+        console.log(`로그인성공`, userInfo);
+      })
+      .catch((e) => {
+        alert(e);
+        console.log(`로그인 실패`, userInfo);
+      });
   };
 
   // async function loginWithEamil(email, password) {
@@ -42,18 +58,13 @@ export default function Login() {
           <div className="flex justify-center mb-[30px]">
             <img src={FooLogo} alt="logo" />
           </div>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              // loginWithEamil(email, password);
-            }}
-          >
+          <form onSubmit={handleSubmit}>
             <div>
               <input
                 type="text"
                 placeholder="ID를 입력하세요."
                 className="w-full border p-2 text-[14px]"
-                onChange={handleEmailChange}
+                onChange={handleEmail}
               />
             </div>
             <div className="mt-[10px]">
@@ -62,7 +73,7 @@ export default function Login() {
                 placeholder="PASSWORD를 입력하세요."
                 className="w-full border p-2 text-[14px]"
                 autoComplete="new-password"
-                onChange={handlePasswordChange}
+                onChange={handlePwd}
               />
             </div>
             <LoginButton>로그인</LoginButton>
