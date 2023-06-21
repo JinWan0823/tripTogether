67cec12836a054670e8491372d831082a9f2c5d8
jinWan0823 <dhksl823@gmail.com>
2023-06-21 14:18:47 +0900

    회원가입 유효성 검사

--- trip-together/src/pages/SignUp.tsx
+++ trip-together/src/pages/SignUp.tsx
@@ -6,11 +6,18 @@ import FooLogo from "../assets/logo.svg";
 import { AuthContext } from "../context/authContext";
 import { createUserWithEmailAndPassword } from "@firebase/auth";
 import { auth } from "../firebase";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
   const userInfo = useContext(AuthContext);
   const [email, setEmail] = useState("");
   const [pwd, setPwd] = useState("");
+  const [pwd2, setPwd2] = useState("");
+
+  const [errorMessage, setError] = useState("");
+
+  const navigate = useNavigate();
 
   const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -21,16 +28,42 @@ export default function SignUp() {
     e.preventDefault();
     setPwd(e.target.value);
   };
+  const handlePwd2 = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setPwd2(e.target.value);
+  };
+
+  // const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   e.preventDefault();
+  //   setName(e.target.value);
+  // };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
+    if (pwd !== pwd2) {
+      setError("비밀번호가 같지 않습니다.");
+      return;
+    }
+
     createUserWithEmailAndPassword(auth, email, pwd)
       .then(() => {
         alert("회원가입 성공");
+        navigate("/");
       })
       .catch((e) => {
-        alert(e);
+        console.log(e.code);
+        switch (e.code) {
+          case "auth/email-already-in-use":
+            setError("이미 사용중인 이메일입니다.");
+            break;
+          case "auth/invalid-email":
+            setError("유효하지 않은 이메일입니다.");
+            break;
+          case "auth/weak-password":
+            setError("비밀번호가 6자리 이상 필요합니다.");
+            break;
+        }
       });
   };
 
@@ -41,30 +74,65 @@ export default function SignUp() {
         className="min-h-screen relative bg-cover"
         style={{ backgroundImage: `url(${back})` }}
       >
-        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white w-[400px] p-[30px] py-[50px]">
+        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-white w-[400px] p-[30px] py-[50px] rounded-[10px]">
           <div className="flex justify-center mb-[30px]">
             <img src={FooLogo} alt="logo" />
           </div>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <p>ID</p>
+          <form
+            onSubmit={handleSubmit}
+            className="flex items-center justify-center flex-col"
+          >
+            {/* <div className="mt-[10px] w-full">
+              <p>이름</p>
               <input
                 type="text"
-                placeholder="ID를 입력하세요."
+                placeholder="이름을 입력하세요."
+                className="w-full border p-2 text-[14px]"
+                onChange={handleName}
+                required
+              />
+            </div> */}
+            <div className="w-full">
+              <p>E-mail</p>
+              <input
+                type="text"
+                placeholder="이메일을 입력하세요."
                 className="w-full border p-2 text-[14px]"
                 onChange={handleEmail}
+                required
               />
             </div>
-            <div className="mt-[10px]">
+            <div className="mt-[10px] w-full">
               <p>비밀번호</p>
               <input
                 type="password"
-                placeholder="PASSWORD를 입력하세요."
+                placeholder="비밀번호를 입력하세요."
                 className="w-full border p-2 text-[14px]"
                 onChange={handlePwd}
+                required
               />
             </div>
+            <div className="mt-[10px] w-full">
+              <p>비밀번호 확인</p>
+              <input
+                type="password"
+                placeholder="비밀번호를 입력하세요."
+                className="w-full border p-2 text-[14px]"
+                onChange={handlePwd2}
+                required
+              />
+            </div>
+            <p
+              className={`text-[14px] mt-[5px] text-red-500 ${
+                errorMessage ? "opacity-1" : "opacity-0"
+              }`}
+            >
+              * {errorMessage}
+            </p>
             <LoginButton>회원가입</LoginButton>
+            <Link to={"/login"} className="text-[#666] underline mt-[5px]">
+              로그인
+            </Link>
           </form>
         </div>
       </div>
