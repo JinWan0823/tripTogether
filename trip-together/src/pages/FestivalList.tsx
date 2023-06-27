e1b69794fd6b142b0097a229d624a06274b77e0d
jinWan0823 <dhksl823@gmail.com>
2023-06-27 13:49:18 +0900

    여행/축제 리스트 페이지

---
+++ trip-together/src/pages/FestivalList.tsx
@@ -0,0 +1,20 @@
+import SubBanner from "../components/Subpage/SubBanner";
+import TravelTotalList from "../components/Subpage/TravelTotalList";
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
+import firstimage from "../assets/sub/banner.png";
+import Weather from "../components/common/Weater";
+import UpButton from "../components/common/UpButton";
+
+export default function FestivalList() {
+  return (
+    <>
+      <Header />
+      <SubBanner firstimage={firstimage} />
+      <Weather />
+      <TravelTotalList />
+      <Footer />
+      <UpButton />
+    </>
+  );
+}
